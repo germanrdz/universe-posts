fd2979299bc292e2f055343601f6e85829b9e3c1
German Rodriguez <germanson@gmail.com>
2022-08-03 14:20:56 -0700

    Separating Navigatir into its own component

---
+++ src/components/shared/Navigator.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { useNavigate } from 'react-router-dom';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 16px;
+
+  button {
+    font-size: 3em;
+    color: #FFF;
+    
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    text-decoration: none;
+    display: inline;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+function Navigator () {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <button onClick={() => navigate(-1)}>{'<'}</button>
+      <button onClick={() => navigate(1)}>{'>'}</button>
+    </Container>
+  )
+}
+
+export default Navigator;
--- src/components/shared/Title.js
+++ src/components/shared/Title.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
 
+import Navigator from "./Navigator";
 import { H1 } from  "./Text";
 
 const Container = styled.div`
@@ -9,35 +9,11 @@ const Container = styled.div`
   align-items: baseline;
 `;
 
-const Navigator = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 16px;
-
-  button {
-    font-size: 3em;
-    color: #FFF;
-    
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    text-decoration: none;
-    display: inline;
-    margin: 0;
-    padding: 0;
-  }
-`;
-
 function Title ({ text }) {
-  const navigate = useNavigate();
-
   return (
     <Container>
       <H1>{ text }</H1>
-      <Navigator>
-        <button onClick={() => navigate(-1)}>{'<'}</button>
-        <button onClick={() => navigate(1)}>{'>'}</button>
-      </Navigator>
+      <Navigator />
     </Container>
   );
 };
